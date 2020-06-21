4bc5be8764a3f4d33891165c3786dc1088a78324
Arun Agarwal <arun.agarwal@nagarro.com>
2020-06-21 10:53:51 +0530

    Fix | Hide join room tab when room joined.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../Services/user.service';
 import { Router } from '@angular/router';
+import { ChatService } from '../Services/chat.service';
 
 @Component({
   selector: 'app-header',
@@ -10,12 +11,19 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   isLoggedIn: boolean = false;
-  constructor(private router: Router, private userService: UserService) { }
+  isRoomJoined: boolean = false;
+  constructor(private router: Router, private chatService: ChatService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.userService.getIsLoggedInObserver().subscribe(response => {
       this.isLoggedIn = response;
     });
+
+    this.chatService.getRoomNameObserver().subscribe(response =>{
+      if(response !== null){
+        this.isRoomJoined = true;
+      }
+    })
   }
 
   LogOut() {
